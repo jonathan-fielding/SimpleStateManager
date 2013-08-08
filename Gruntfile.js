47a81da9460891b426d3095dbae666bae9bfbb84
Iszak Bryan <iszak.bryan@gmail.com>
2013-08-08 08:08:53 +0100

    Fixed coding style inconsistencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,21 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'js/ssm.js',
+                dest: 'js/ssm.min.js'
+            }
+        }
+    });
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'js/ssm.js',
-        dest: 'js/ssm.min.js'
-      }
-    }
-  });
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
-
+    // Default task(s).
+    grunt.registerTask('default', ['uglify']);
 };
